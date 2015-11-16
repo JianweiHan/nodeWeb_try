c8c8994203528f22db041bdbc12553242297ff13
jhan <jhan0530@gmail.com>
2015-11-16 10:58:52 -0800

    second

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var express = require('express');
-var path = require('path');
+//var path = require('path');
 
 var app = express();
-
+app.set('port', (process.env.PORT || 5000));
 //app.set('view engine', 'ejs');
 //app.set('views', path.join(__dirname, 'views'));
 //app.set('views', './views');
@@ -15,10 +15,13 @@ app.get('/', function(req, res){
 	//res.render('index');
 });
 
-app.listen(1337, function(){
-	console.log('ready on port 1337');
-});
+// app.listen(1337, function(){
+// 	console.log('ready on port 1337');
+// });
 
+app.listen(app.get('port'), function(){
+	console.log('Node app is running on port', app.get('port'));
+});
 
 
 
